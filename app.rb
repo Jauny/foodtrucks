b9569d646efbd039a82effc02e1d0b6ad89410be
Jonathan Pepin <jypepin@gmail.com>
2014-01-16 16:43:23 -0800

    bootstrap

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ class App < Sinatra::Base
 
   get '/' do
     @trucks = HTTParty.get("http://data.sfgov.org/resource/rqzj-sfat.json")
-    @trucks.select! {|truck| !!truck["location"] && truck["status"] == "APPROVED"}
+    @trucks.select! { |truck| !!truck["location"] && truck["status"] == "APPROVED" }
 
     haml :index
   end
