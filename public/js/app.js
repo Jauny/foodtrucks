2a4a51465cf33a02acdb7f824877ec6b69f65630
Jonathan Pepin <jypepin@gmail.com>
2014-01-19 18:06:00 -0800

    rerender correct filtered pins

--- public/js/app.js
+++ public/js/app.js
@@ -146,13 +146,14 @@ $(function () {
 
   // Map view
   App.Views.MapView = Backbone.View.extend({
-    el: $('#map-canvas'),
+    el: '#map-canvas',
 
     initialize: function() {
       // adds the event add to add an element to the collection
       this.listenTo(this.collection, 'add', this.addOne);
       this.listenTo(this.collection, 'reset', this.render);
       this.listenTo(this.collection, 'closePins', this.closePins);
+      this.listenTo(this.collection, 'filter', this.renderFiltered);
 
       var mapOptions = {
         center: new google.maps.LatLng(37.7745948, -122.4127949),
@@ -182,6 +183,15 @@ $(function () {
 
     render: function() {
       this.collection.forEach(this.addOne, this);
+      return this;
+    },
+
+    renderFiltered: function() {
+      _.each(this.pinViews, function(pin) {
+        pin.marker.setMap(null);
+        pin.remove();
+      });
+      this.collection.filter(query).forEach(this.addOne, this);
       return this;
     },
 
